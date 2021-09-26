eebcd29405be9d8c32401c0ddfb4aaa42e6c5118
chaeshee0908 <chaeshee0908@naver.com>
2021-09-26 19:40:26 +0900

    3주차

---
+++ chaeshee0908/조이스틱.py
@@ -0,0 +1,36 @@
+# 그리디 알고리즘
+# 실패
+
+def make_A(alphabet):
+    diff = ord(alphabet) - ord('A')
+    if diff > 13:
+         return (26 - diff)
+    else:
+        return diff
+
+def solution(name):
+    place_not_A = []
+    move = 0
+
+    # 문자중 'A' 아닌 곳 위치 파악
+    for i in range(1,len(name)):
+        if name[i] != 'A':
+            place_not_A.append(i)
+    
+    # 첫 알파벳 'A'로 바꾸기
+    if name[0] != 'A':
+        move += make_A(name[0])
+
+    # 조이스틱을 오른쪽 방향으로 가는 것이 더 가까울 때
+    if place_not_A[0] <= len(name) - place_not_A[-1]:
+        for i in range(1, len(name)):
+            move += 1
+            if name[i] != 'A':
+                move += make_A(name[i])
+    # 조이스틱을 왼쪽 방향으로 가는 것이 더 가까울 때
+    else:
+        for i in range(len(name)-1, 1, -1):
+            move += 1
+            if name[i] != 'A':
+                move += make_A(name[i])
+    return move
