e7a15dab8e0efdac6749b07c3e25f471467a0ef7
chaeshee0908 <chaeshee0908@naver.com>
2021-09-26 19:37:04 +0900

    1주차

---
+++ chaeshee0908/거리두기확인하기.py
@@ -0,0 +1,41 @@
+# bfs, dfs
+
+from collections import deque
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(room, x, y, d):
+    visited = [[False]*5 for _ in range(5)]
+    queue = deque()
+    queue.append([x, y, d])
+    while queue:
+        x, y, d = queue.popleft()
+        visited[x][y] = True
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            nd = d + 1
+
+            if 0 <= nx < 5 and 0 <= ny < 5 and not visited[nx][ny]:
+                visited[nx][ny] = True
+
+                if room[nx][ny] == 'P':
+                    if nd <= 2:
+                        return False
+                elif room[nx][ny] == 'O':
+                    queue.append([nx, ny, nd])
+    return True
+
+def solution(places):
+    answer = []
+    for room in places:
+        flag = 1
+        for i in range(5):
+            for j in range(5):
+                if room[i][j] == 'P':
+                    if bfs(room, i, j, 0) == False:
+                        flag = 0
+        answer.append(flag)
+    return answer
--- chaeshee0908/다리를지나는트럭.py
+++ chaeshee0908/다리를지나는트럭.py
@@ -2,7 +2,7 @@ def solution(bridge_length, weight, truck_weights):
     time = 0
     first = sum(truck_weights)
     in_bridge= [0] * bridge_length
-		// 다리를 지나기 전 트럭이 남아있는 경우
+    # 다리를 지나기 전 트럭이 남아있는 경우
     while truck_weights:
         if sum(in_bridge):
             for truck in in_bridge:
@@ -27,14 +27,14 @@ def solution(bridge_length, weight, truck_weights):
             time += 1
             del truck_weights[0]
 
-		// 다리 위에 트럭이 남아있는 경우
+    # 다리 위에 트럭이 남아있는 경우
     while in_bridge:
-				// 다리 위에 트럭이 남아있지 않은 경우
-	      if sum(in_bridge) == 0:
+        # 다리 위에 트럭이 남아있지 않은 경우
+        if sum(in_bridge) == 0:
             break
         else:
             for truck in in_bridge:
-								// 가장 선두의 트럭을 찾음
+                # 가장 선두의 트럭을 찾음
                 if truck != 0:
                     time += 1
                     if in_bridge.index(truck) == 0:
